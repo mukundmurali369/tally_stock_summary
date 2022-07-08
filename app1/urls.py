135b2f0a55e347edb62a5cf4598aeabb66d8dfc8
mukundmurali369 <mukundpolicodu@gmail.com>
2022-07-08 16:51:23 +0530

    Third commit

--- app1/urls.py
+++ app1/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path('',views.base,name='base'),
     path('load_stock_summarys',views.load_stock_summarys,name='load_stock_summarys'),
     path('load_stock_group',views.load_stock_group,name='load_stock_group'),
+    path('load_item_details',views.load_item_details,name='load_item_details'),
+    path('load_stock_voucher',views.load_stock_voucher,name='load_stock_voucher'),
 
 ]
--- app1/views.py
+++ app1/views.py
@@ -5,10 +5,16 @@ from .models import *
 # Create your views here.
 def base(request):
     return render(request, 'base.html')
-    
+
 def load_stock_summarys(request):
     return render(request, 'load_stock_summarys.html')
 
 def load_stock_group(request):
     return render(request, 'load_stock_group.html')
+
+def load_item_details(request):
+    return render(request, 'load_item_details.html')
+
+def load_stock_voucher(request):
+    return render(request, 'load_stock_voucher.html')
 
